647bbb59039c2514679ab6e521fcd2f17e8aebf0
George Shakhnazaryan <gshakhn@gmail.com>
2014-09-10 21:35:14 -0500

    Don't export BinderPicker.scala to JS since we only call it from scala code.

--- client/src/main/scala/com/gshakhn/privatewiki/client/BinderPicker.scala
+++ client/src/main/scala/com/gshakhn/privatewiki/client/BinderPicker.scala
@@ -5,7 +5,6 @@ import org.scalajs.dom.HTMLDivElement
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.all._
 
-@JSExport
 object BinderPicker {
   def addPicker(element: HTMLDivElement): Unit = {
     element.appendChild(input(tpe:="text").render)
