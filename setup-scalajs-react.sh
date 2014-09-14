0e1cd933f2053768f2a63b4cfaea6bb4fd5ec30d
George Shakhnazaryan <gshakhn@gmail.com>
2014-09-13 21:17:27 -0500

    Clone using git:// protocol.
    Apparently travis doesn't have SSH keys to clone.
    
    Also, cd out a few directories before cloning.

--- setup-scalajs-react.sh
+++ setup-scalajs-react.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git clone git@github.com:japgolly/scalajs-react.git
+cd ../../
+git clone git://github.com/japgolly/scalajs-react.git scalajs-react
 cd scalajs-react
 git checkout 669f87e9f2010f30929e5b2a09facd2872438c3b # known good version
 sbt publish-local
-cd ../
+cd ../gshakhn/private-wiki
