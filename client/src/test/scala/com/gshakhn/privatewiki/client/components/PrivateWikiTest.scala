169886c97ca128b79fee61c10ef5a9b56192b1b4
George Shakhnazaryan <gshakhn@gmail.com>
2014-09-14 17:03:15 -0500

    Get rid of tests in PrivateWikiTest.
    They were mainly asserting on style, which isn't super helpful.

--- client/src/test/scala/com/gshakhn/privatewiki/client/components/PrivateWikiTest.scala
+++ client/src/test/scala/com/gshakhn/privatewiki/client/components/PrivateWikiTest.scala
@@ -16,32 +16,5 @@ object PrivateWikiTest extends TestSuite {
   React.renderComponent(PrivateWiki(), containingDiv)
   
   def tests = TestSuite {
-    "container div"- {
-      val mainContainer = jQuery(containingDiv).find("div#mainContainer")
-      "should exist"- {
-        assert(mainContainer.length == 1)
-      }
-      "should be styled with bootstrap"- {
-        assert(mainContainer.hasClass("container"))
-      }
-      "first row"- {
-        val firstRow = mainContainer.find("div#row-1")
-        "should exist"- {
-          assert(firstRow.length == 1)
-        }
-        "should be styled with bootstrap"- {
-          assert(firstRow.hasClass("row"))
-        }
-        "first column"- {
-          val firstCol = mainContainer.find("div#col-1-1")
-          "should exist"- {
-            assert(firstCol.length == 1)
-          }
-          "should be styled with bootstrap"- {
-            assert(firstCol.hasClass("col-md-4"))
-          }
-        }
-      }
-    }
   }
 }
