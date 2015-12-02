a4539752192955b22680cadc9d62647f6615ab7d
George Shakhnazaryan <gshakhn@gmail.com>
2015-12-02 13:42:00 +0700

    Allow selection of a binder in PaperList.

--- client/src/main/scala/com/gshakhn/privatewiki/client/components/PaperList.scala
+++ client/src/main/scala/com/gshakhn/privatewiki/client/components/PaperList.scala
@@ -6,24 +6,49 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 
 object PaperList {
 
-  case class Props(binders: Seq[UnlockedBinder])
+  case class Props(binders: Seq[AnyBinderButton])
+  case class State(selectedButton: AnyBinderButton)
+
+  trait AnyBinderButton {
+    def name: String
+  }
+
+  case object AllBinders extends AnyBinderButton {
+    override def name: String = "All"
+  }
+
+  case class BinderButton(binder: UnlockedBinder) extends AnyBinderButton {
+    override def name: String = binder.name
+  }
+
+  class Backend($: BackendScope[Props, State]) {
+    def selectNewBinder(newSelectedButton: AnyBinderButton): Callback = {
+      $.modState(s => s.copy(selectedButton = newSelectedButton))
+    }
+  }
 
   private[this] val component = ReactComponentB[Props]("PaperList")
-    .render_P(
-      props =>
+    .initialState(State(AllBinders))
+    .backend(new Backend(_))
+    .renderPS(
+      ($, props, state) =>
         <.div(
           ^.cls := "paper-list",
           <.div(
             ^.cls := "binder-list btn-group",
-            <.div(
-              ^.cls := "btn btn-default active",
-              "All"
-            )
+            props.binders.map { btn =>
+              <.div(
+                ^.classSet1("btn btn-default",
+                  "active" -> (state.selectedButton == btn)),
+                ^.onClick --> $.backend.selectNewBinder(btn),
+                btn.name
+              )
+            }
           )
         )).build
 
-  def apply(binders: Seq[UnlockedBinder]): ReactComponentU[Props, Unit, Unit, TopNode] = {
-    component(Props(binders))
+  def apply(binders: Seq[UnlockedBinder]): ReactComponentU[Props, State, Backend, TopNode] = {
+    component(Props(Seq(AllBinders) ++ binders.map(BinderButton.apply)))
   }
 }
 
--- client/src/test/scala/com/gshakhn/privatewiki/client/components/PaperListSpec.scala
+++ client/src/test/scala/com/gshakhn/privatewiki/client/components/PaperListSpec.scala
@@ -2,6 +2,7 @@ package com.gshakhn.privatewiki.client.components
 
 import com.gshakhn.privatewiki.client.UnlockedBinder
 import japgolly.scalajs.react.ReactDOM
+import japgolly.scalajs.react.test.ReactTestUtils
 import org.scalajs.jquery._
 import org.scalatest.path
 
@@ -16,14 +17,14 @@ class PaperListSpec extends ReactJsBaseSpec {
       describe("renders") {
         render
 
-        describe("the main div") {
+        describe("the main div that") {
           val div = jQuery("div.paper-list")
 
           it("exists") {
             div.length shouldBe 1
           }
 
-          describe("with a binder list that") {
+          describe("has a binder list that") {
             val binderList = div.find("div.binder-list")
 
             it("exists") {
@@ -39,19 +40,91 @@ class PaperListSpec extends ReactJsBaseSpec {
               binders.length shouldBe 1
             }
 
-            it("has a binder titled 'All'") {
-              val binders = binderList.find(".btn")
+            it("has a button titled 'All'") {
+              val binders = binderList.find("div:contains('All').btn")
+              binders.length shouldBe 1
               binders(0).textContent shouldBe "All"
             }
 
             it("has the 'All' button active") {
-              val binders = binderList.find(".btn")
+              val binders = binderList.find("div:contains('All').btn")
               binders.eq(0) should haveClass("active")
             }
           }
         }
       }
     }
+
+    describe("with 1 binder") {
+      implicit val binders: Seq[UnlockedBinder] = Seq(UnlockedBinder("binder", Set.empty))
+
+      describe("renders") {
+        render
+
+        describe("the main div that") {
+          val div = jQuery("div.paper-list")
+
+          it("exists") {
+            div.length shouldBe 1
+          }
+
+          describe("has a binder list that") {
+            val binderList = div.find("div.binder-list")
+
+            it("exists") {
+              binderList.length shouldBe 1
+            }
+
+            it("is styled with bootstrap") {
+              binderList should haveClass("btn-group")
+            }
+
+            it("has 2 binders") {
+              val binders = binderList.find(".btn")
+              binders.length shouldBe 2
+            }
+
+            it("has a button titled 'All'") {
+              val binders = binderList.find("div:contains('All').btn")
+              binders.length shouldBe 1
+              binders(0).textContent shouldBe "All"
+            }
+
+            it("has a button titled with the binder name") {
+              val binders = binderList.find("div:contains('binder').btn")
+              binders.length shouldBe 1
+              binders(0).textContent shouldBe "binder"
+            }
+
+            it("has the 'All' button active") {
+              val binders = binderList.find("div:contains('All').btn")
+              binders.eq(0) should haveClass("active")
+            }
+
+            it("has the other button not active") {
+              val binders = binderList.find("div:contains('binder').btn")
+              binders.eq(0) shouldNot haveClass("active")
+            }
+
+            describe("when the binder button is clicked") {
+              val binder = binderList.find("div:contains('binder').btn").get(0)
+              ReactTestUtils.Simulate.click(binder)
+
+              it("marks the 'All' button inactive") {
+                val binders = binderList.find("div:contains('All').btn")
+                binders.eq(0) shouldNot haveClass("active")
+              }
+
+              it("marks the binder button active") {
+                val binders = binderList.find("div:contains('binder').btn")
+                binders.eq(0) should haveClass("active")
+              }
+            }
+          }
+        }
+      }
+    }
+
   }
 
   tearDown()
