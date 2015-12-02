2995191fc842da4b333ad998c68c58acea732887
George Shakhnazaryan <gshakhn@gmail.com>
2015-12-02 13:52:22 +0700

    Move PaperList render method into its Backend.

--- client/src/main/scala/com/gshakhn/privatewiki/client/components/PaperList.scala
+++ client/src/main/scala/com/gshakhn/privatewiki/client/components/PaperList.scala
@@ -25,27 +25,29 @@ object PaperList {
     def selectNewBinder(newSelectedButton: AnyBinderButton): Callback = {
       $.modState(s => s.copy(selectedButton = newSelectedButton))
     }
+
+    def render(props: Props, state: State): ReactElement = {
+      <.div(
+        ^.cls := "paper-list",
+        <.div(
+          ^.cls := "binder-list btn-group",
+          props.binders.map { btn =>
+            <.div(
+              ^.classSet1("btn btn-default",
+                "active" -> (state.selectedButton == btn)),
+              ^.onClick --> selectNewBinder(btn),
+              btn.name
+            )
+          }
+        )
+      )
+    }
   }
 
   private[this] val component = ReactComponentB[Props]("PaperList")
     .initialState(State(AllBinders))
-    .backend(new Backend(_))
-    .renderPS(
-      ($, props, state) =>
-        <.div(
-          ^.cls := "paper-list",
-          <.div(
-            ^.cls := "binder-list btn-group",
-            props.binders.map { btn =>
-              <.div(
-                ^.classSet1("btn btn-default",
-                  "active" -> (state.selectedButton == btn)),
-                ^.onClick --> $.backend.selectNewBinder(btn),
-                btn.name
-              )
-            }
-          )
-        )).build
+    .renderBackend[Backend]
+    .build
 
   def apply(binders: Seq[UnlockedBinder]): ReactComponentU[Props, State, Backend, TopNode] = {
     component(Props(Seq(AllBinders) ++ binders.map(BinderButton.apply)))
